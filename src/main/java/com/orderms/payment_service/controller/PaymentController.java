a36e7de36426ccd66574060f57f101c15017520e
vamsi <vamsiyerra42@gmail.com>
2026-09-09 07:54:28 +0530

    Phase 3 - Step 3 - Established API Gateway and GlobalLoggingFIlter

--- src/main/java/com/orderms/payment_service/controller/PaymentController.java
+++ src/main/java/com/orderms/payment_service/controller/PaymentController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/payments")
+@RequestMapping("/api/payments")
 @RequiredArgsConstructor
 public class PaymentController {
 
